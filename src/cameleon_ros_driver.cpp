f78300f8cd50f8625ea2899917229e4324fcf73c
Tom Krajnik <tkrajnik@lincoln.ac.uk>
2017-04-13 15:48:50 +0100

    Now publishing odometry

--- src/cameleon_ros_driver.cpp
+++ src/cameleon_ros_driver.cpp
@@ -1,10 +1,13 @@
 #include <ros/ros.h>
 #include <tf/tf.h>
 #include <geometry_msgs/Twist.h>
+#include <nav_msgs/Odometry.h>
 #include "CMessageClient.h"
 #include <string.h>
+#include <boost/thread/thread.hpp>
 
 ros::Subscriber commandSub;
+ros::Publisher odometryPub;
 
 //robot Parameters
 int robotPort;
@@ -14,19 +17,69 @@ double forwardSpeedGain,turnSpeedGain;
 CMessage message;
 CMessageClient client;
 CStatusMessage status;
+nav_msgs::Odometry odometry;
+
+bool lastOdoValid = false;
+double px = 0;
+double py = 0;
+double heading = 0;
+double lastOdoLeft = 0;
+double lastOdoRight = 0;
+
+void updateOdometry(CStatusMessage status)
+{
+	double odoLeft = 0.001 * (double) status.odoLeft;
+	double odoRight = 0.001 * (double) status.odoRight;
+	if (lastOdoValid) {
+		double wheelBase = 0.40;
+		double dLeft = odoLeft - lastOdoLeft;
+		double dRight = odoRight - lastOdoRight;
+		double dForward = 0.5 * ( dLeft + dRight );
+		double dHeading = (dRight - dLeft) / wheelBase;
+
+		heading += dHeading;
+		px += dForward * cos(heading);
+		py += dForward * sin(heading);
+
+		odometry.pose.pose.position.x = px;
+		odometry.pose.pose.position.y = py;
+		tf::Quaternion orientation;
+	        orientation.setRPY((float)status.roll*M_PI/180,(float)status.pitch*M_PI/180,heading);
+		odometry.pose.pose.orientation.x = orientation[0];
+		odometry.pose.pose.orientation.y = orientation[1];
+		odometry.pose.pose.orientation.z = orientation[2];
+		odometry.pose.pose.orientation.w = orientation[3];
+		odometryPub.publish(odometry); 
+	}
+	lastOdoLeft = odoLeft;
+	lastOdoRight = odoRight;
+	lastOdoValid = true;
+}
+
+void readData()
+{
+	CStatusMessage status;
+	while (ros::ok())
+	{
+		if (client.checkForHeader() == 0) {
+			if (client.checkForStatus(status) == 0){
+				updateOdometry(status);
+			}
+		}
+	}
+}
 
 void commandCallback(const geometry_msgs::Twist::ConstPtr& msg)
 {
 	message.type = MSG_SPEED;
 	message.forward = (int)(msg->linear.x*1000);
 	message.turn = (int)(msg->angular.z*1000);
-	message.flipper = 0;
+	message.flipper = (int)(msg->angular.y*1000);
 
 	if (message.forward > +maxForwardSpeed*1000) message.forward = maxForwardSpeed*1000;
 	if (message.forward < -maxBackwardSpeed*1000) message.forward = -maxBackwardSpeed*1000;
 	if (message.turn    > +maxTurnSpeed*1000) message.turn = maxTurnSpeed*1000;
 	if (message.turn    < -maxTurnSpeed*1000) message.turn = -maxTurnSpeed*1000;
-
 	client.sendMessage(message);
 }
      
@@ -45,8 +98,10 @@ int main(int argc, char** argv)
 	bool requier[2] = {true, true};
 	client.init(robotIP.c_str(), robotPort, requier);
 
+	boost::thread thread_b(readData);
+
 	//create the robot
-	//cmd_vel = n.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
+	odometryPub = n.advertise<nav_msgs::Odometry>("/odom", 1);
 	commandSub = n.subscribe("/cmd_vel", 1, commandCallback);
 
 	ros::spin();
