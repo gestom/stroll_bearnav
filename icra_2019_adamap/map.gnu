36297ffdf3ff5e0347d271fb36439fd3a51bc50b
Tom Krajnik <tkrajnik@labe.felk.cvut.cz>
2018-09-09 10:35:31 +0200

    All

--- icra_2019_adamap/map.gnu
+++ icra_2019_adamap/map.gnu
@@ -9,3 +9,5 @@ plot [0:29] [:]\
 'results/Map_plastic.srt' using 1:(1-$0/A_records) with lines title "Plastic map: new map every traverse" lw 1,\
 'results/Map_static.srt' using 1:(1-$0/A_records) with lines title "Static map: no adaptation to changes" lw 1,\
 'results/Map_adaptive.srt' using 1:(1-$0/A_records) with lines title "Adaptive map:  gradual adaptation to changes" lw 1,\
+'results/Map_adaptive_fixed_1.srt' using 1:(1-$0/A_records) with lines title "Adaptive map 1" lw 1,\
+'results/Map_adaptive_fixed_6.srt' using 1:(1-$0/A_records) with lines title "Adaptive map " lw 1,\
--- icra_2019_adamap/process.sh
+++ icra_2019_adamap/process.sh
@@ -9,38 +9,16 @@ esac
 mkdir "results"
 
 f="$1/icra_2019_adamap/long"
-
-confidence=0.05
-if [ 0 == 1 ]
+if [ 0 == 1 ] 
 then
-############ Map adaptation test (Section 4.4)
-rosparam set names_map  [A000,M000]
-rosparam set names_view [A000]
-rosrun dynamic_reconfigure dynparam set /navigator "{'summaryMap': False,'plasticMap': True,'histogramRating': False,'remapRotGain': 0.0}"&
-roslaunch stroll_bearnav remapTest.launch folder_map:=$f folder_view:=$f
-cp $f/A000_GT.txt $f/M000_GT.txt
-fi
-
-if [ 0 == 1 ]
-then
-#test the map update schemes
-rosrun dynamic_reconfigure dynparam set /navigator "{'summaryMap': False, 'plasticMap': False,'histogramRating': False,'remapRotGain': 1.0}"&
-rosparam set names_map  [$(for i in $(seq -w 1 87);do echo -ne M000,;done)]
-rosparam set names_view [$(for i in $(seq -w 1 87);do echo -ne A0$i,;done)]
-roslaunch stroll_bearnav evaluate.launch folder_map:=$f folder_view:=$f
-cp ~/.ros/Results.txt results/Map_static.txt
-
-rosrun dynamic_reconfigure dynparam set /navigator "{'summaryMap': False, 'plasticMap': True,'histogramRating': False,'remapRotGain': 1.0}"&
-rosparam set names_map  [$(echo -ne "M000,";for i in $(seq -w 1 87);do echo -ne B0$i,;done)]
-rosparam set names_view [$(for i in $(seq -w 1 87);do echo -ne A0$i,;done)]
-roslaunch stroll_bearnav remapTest.launch folder_map:=$f folder_view:=$f
-cp ~/.ros/Results.txt results/Map_plastic.txt
-
-rosrun dynamic_reconfigure dynparam set /navigator "{'summaryMap': False, 'plasticMap': False,'histogramRating': False,'remapRotGain': 1.0}"&
+for i in $(seq 1 5 10);
+do 
+rosrun dynamic_reconfigure dynparam set /navigator "{'summaryMap': False, 'plasticMap': False,'histogramRating': False,'remapRotGain': 1.0,'maxFeatureRemap': $i,'minFeatureRemap': $i }"&
 rosparam set names_map  [$(echo -ne "M000,";for i in $(seq -w 1 87);do echo -ne C0$i,;done)]
 rosparam set names_view [$(for i in $(seq -w 1 87);do echo -ne A0$i,;done)]
 roslaunch stroll_bearnav remapTest.launch folder_map:=$f folder_view:=$f
-cp ~/.ros/Results.txt results/Map_adaptive.txt
+cp ~/.ros/Results.txt results/Map_adaptive_fixed_$i.txt
+done
 fi
 
 path=`pwd`
@@ -51,13 +29,11 @@ cd $path
 
 echo "in `pwd`"
 
-for i in Map_weighted Map_adaptive Map_plastic Map_static;do grep reports results/$i.txt|awk '{a=$21-$23;b=(sqrt(a*a)+384)%768-384;print sqrt(b*b)}'| tee results/$i.err|sort -nr > results/$i.srt;done
+for i in Map_adaptive_fixed_1 Map_adaptive_fixed_6;do grep reports results/$i.txt|awk '{a=$21-$23;b=(sqrt(a*a)+384)%768-384;print sqrt(b*b)}'| tee results/$i.err|sort -nr > results/$i.srt;done
 echo MAP PLASTICITY TEST: Section 4.4
-echo -ne "	Error of Adaptive VS Static: "
-paste results/Map_adaptive.err results/Map_static.err          |./icra_2019_adamap/t-test $confidence
-echo -ne "	Error of Adaptive VS Plastic: "
-paste results/Map_adaptive.err results/Map_plastic.err          |./icra_2019_adamap/t-test $confidence
-echo -ne "	Error of Plastic VS Static: "
-paste results/Map_plastic.err results/Map_static.err |./icra_2019_adamap/t-test $confidence
+echo -ne "	Error of 1 VS 5: "
+paste results/Map_adaptive_fixed_1.err results/Map_adaptive_fixed_6.err          |./icra_2019_adamap/t-test $confidence
+echo -ne "	Error of 5 VS 0.5: "
+paste results/Map_adaptive_fixed_6.err results/Map_adaptive.err          |./icra_2019_adamap/t-test $confidence
 echo
 gnuplot icra_2019_adamap/map.gnu >results/map.fig
