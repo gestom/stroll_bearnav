231e313c3ecb304b62e4df9f00f4d0710856e36d
Tom Krajnik <tkrajnik@labe.felk.cvut.cz>
2018-09-16 10:57:24 +0200

    Strict

--- icra_2019_adamap/map.gnu
+++ icra_2019_adamap/map.gnu
@@ -10,3 +10,4 @@ plot [0:29] [:]\
 'results/Map_static.srt' using 1:(1-$0/A_records) with lines title "Static map: no adaptation to changes" lw 1,\
 'results/Map_adaptive.srt' using 1:(1-$0/A_records) with lines title "Adaptive map:  gradual adaptation to changes" lw 1,\
 'results/Map_adaptive_aggresive.srt' using 1:(1-$0/A_records) with lines title "Adaptive aggr:  gradual adaptation to changes" lw 1,\
+'results/Map_adaptive_strict.srt' using 1:(1-$0/A_records) with lines title "Adaptive strict:  gradual adaptation to changes" lw 1,\
--- src/navigator.cpp
+++ src/navigator.cpp
@@ -463,8 +463,8 @@ void featureCallback(const stroll_bearnav::FeatureArray::ConstPtr& msg)
 		info.mapMatchIndex.clear();
 		vector<int> mapIndex(mapFeatures.feature.size());
 		vector<int> mapEval(mapFeatures.feature.size());
-		std::fill(mapIndex.begin(),mapIndex.end(),-1);
-		std::fill(mapEval.begin(),mapEval.end(),-1);
+		std::fill(mapIndex.begin(),mapIndex.end(),0);
+		std::fill(mapEval.begin(),mapEval.end(),0);
 		for (int i = 0;i<good_matches.size();i++)
 		{
 			mapIndex[good_matches[i].queryIdx] = good_matches[i].trainIdx;
@@ -490,7 +490,7 @@ void featureCallback(const stroll_bearnav::FeatureArray::ConstPtr& msg)
 				}else{
 					sort(mapFeatures.feature.begin(), mapFeatures.feature.end(), compare_rating);
 					numFeatureRemove = numFeatureAdd = 0; 
-					while (mapFeatures.feature[mapFeatures.feature.size()-1-numFeatureRemove].rating <= 0 && numFeatureRemove < mapFeatures.feature.size()) numFeatureRemove++;
+					while (mapFeatures.feature[mapFeatures.feature.size()-1-numFeatureRemove].rating < 0 && numFeatureRemove < mapFeatures.feature.size()) numFeatureRemove++;
 					numFeatureAdd = numFeatureRemove;
 
 					mapFeatures.feature.erase(mapFeatures.feature.end() - numFeatureRemove, mapFeatures.feature.end());
