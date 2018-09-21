55a05295367a91019b0dfe68f94cff0fc873f561
Tom Krajnik <tkrajnik@labe.felk.cvut.cz>
2018-09-21 16:19:52 +0200

    Quickie

--- icra_2019_adamap/map.gnu
+++ icra_2019_adamap/map.gnu
@@ -1,12 +1,12 @@
 set terminal fig color
-set size 0.8,0.6
+set size 0.9,0.7
 set key right bottom
-set xlabel 'Registration error [px]'
-set ylabel 'Probability [-]'
+set xlabel 'Registration error threshold [px]'
+set ylabel 'Prob. of correct registration [-]'
 set ytics 0.2
-stats 'results/Map_plastic.srt' prefix "A"
+stats 'results/Map_adaptive_fixed_15.srt' prefix "A"
 plot [0:29] [:]\
-'results/Map_plastic.srt' using 1:(1-$0/A_records) with lines title "Plastic map: new map every traverse" lw 1,\
-'results/Map_static.srt' using 1:(1-$0/A_records) with lines title "Static map: no adaptation to changes" lw 1,\
-'results/Map_adaptive.srt' using 1:(1-$0/A_records) with lines title "Adaptive map:  gradual adaptation to changes" lw 1,\
-'results/Map_adaptive_experiences.srt' using 1:(1-$0/A_records) with lines title "Adaptive map:  gradual adaptation to changes" lw 1,\
+'results/Map_static.srt' using 1:(1-$0/A_records) with lines title "Static map strategy" lw 1 lc 1,\
+'results/Map_plastic.srt' using 1:(1-$0/A_records) with lines title "Latest map strategy" lw 1 lc 2,\
+'results/Map_adaptive_experiences.srt' using 1:(1-10*$0/A_records) with lines title "Multiple map strategy" lw 1 lc 3,\
+'results/Map_adaptive_fixed_30.srt' using 1:(1-$0/A_records) with lines title "Adaptive map strategy (n=30)" lw 1 lc 7\
--- icra_2019_adamap/process.sh
+++ icra_2019_adamap/process.sh
@@ -10,11 +10,11 @@ mkdir "results"
 
 f="$1/icra_2019_adamap/long"
 
-if [ 1 == 1 ]
+if [ 0 == 1 ]
 then
 rosrun dynamic_reconfigure dynparam set /navigator "{'summaryMap': False, 'plasticMap': False,'histogramRating': False,'remapRotGain': 1.0}"&
-rosparam set names_map  [$(echo -ne "M000,";for i in $(seq -w 1 87);do echo -ne C0$i,;done)]
-rosparam set names_view [$(for i in $(seq -w 1 87);do echo -ne A0$i,;done)]
+rosparam set names_map  [$(echo -ne "M000,";for i in $(seq -w 1 10 87);do echo -ne C0$i,;done)]
+rosparam set names_view [$(for i in $(seq -w 1 10 87);do echo -ne A0$i,;done)]
 roslaunch stroll_bearnav remapTest.launch folder_map:=$f folder_view:=$f
 cp ~/.ros/Results.txt results/Map_adaptive_experiences.txt
 fi
@@ -29,12 +29,12 @@ echo "in `pwd`"
 
 for i in $(ls results/Map_adaptive*.txt|sed s/.txt//);do  grep reports $i.txt|awk '($23<5000){a=$21-$23;b=(sqrt(a*a)+384)%768-384;print sqrt(b*b)}'| tee $i.err|sort -nr > $i.srt;done
 
-for i in $(ls results/Map_adaptive*.txt|sed s/.txt//);do 
-echo >$i.tmp
-for j in $(ls results/Map_adaptive*.txt|sed s/.txt//);do 
-paste $j.err $i.err          |./icra_2019_adamap/t-test $confidence >>$i.tmp
-done
-echo "Error $i: " $(grep -c higher $i.tmp) $(grep -c smaller $i.tmp);
-done
+#for i in $(ls results/Map_adaptive*.txt|sed s/.txt//);do 
+#echo >$i.tmp
+#for j in $(ls results/Map_adaptive*.txt|sed s/.txt//);do 
+#paste $j.err $i.err          |./icra_2019_adamap/t-test $confidence >>$i.tmp
+#done
+#echo "Error $i: " $(grep -c higher $i.tmp) $(grep -c smaller $i.tmp);
+#done
 
 gnuplot icra_2019_adamap/map.gnu >results/map.fig
