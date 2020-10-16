f7e783baedc0f39f63a387b994b9d3dd7420ee3c
Marwan Taher <marokhaled99@gmail.com>
2020-10-16 23:01:34 +0100

    lio_sam topics added

--- record_data.sh
+++ record_data.sh
@@ -1,1 +1,1 @@
-rosbag record /zed2/zed_node/rgb/image_rect_color /kodak/panorama /joy /encoder_odom
+rosbag record /zed2/zed_node/rgb/image_rect_color /kodak/panorama /joy /encoder_odom /gnss /imu/data /os1_cloud_node/points
